ba1196ebb5a622976451321a0e102fc93ac65ad6
TheGreat-Red <zburton4825@gmail.com>
2025-05-07 09:30:42 -0400

    emoji fix
    emojis weren't showing properly

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,13 @@
 #include "../include/session.h"
 #include "../include/bankManager.h"
 #include "../include/casinoUi.h"
-
+#include <windows.h>
 Session session;
 AuthManager auth(session);
 Bank bank(session);
 
 int main() {
+    SetConsoleOutputCP(CP_UTF8);
     while (true) {
         if (!auth.authMenu()) return 0;
         if (!session.username.empty()) break;
